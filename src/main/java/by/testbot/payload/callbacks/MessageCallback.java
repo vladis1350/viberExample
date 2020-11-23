125c881ad6708b4284085b1fce2e698238d05b6a
vladis1350 <zerdonik777@gmail.com>
2020-11-23 14:24:38 +0300

    fixed

--- src/main/java/by/testbot/payload/callbacks/MessageCallback.java
+++ src/main/java/by/testbot/payload/callbacks/MessageCallback.java
@@ -21,6 +21,9 @@ public class MessageCallback {
     @JsonProperty("sender")
     private Sender sender;
 
+    @JsonProperty("silent")
+    private Boolean silent;
+
     @JsonProperty("message")
     private Message message;
 
